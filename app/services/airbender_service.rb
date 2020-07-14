0b384c8829cfa3fbc5865a776f2cd3fa91da2276
Derek Borski <derek.borski@gmail.com>
2020-07-14 09:39:58 -0600

    get_characters_from_fire_nation retrieving characters

--- app/services/airbender_service.rb
+++ app/services/airbender_service.rb
@@ -1,26 +1,19 @@
 class AirbenderService
-
-
-
   def get_characters_from_fire_nation
-    params = { affiliation: '', direction: 'desc' }
 
-    get_json('user/repos', github_token, params)
+    get_json('characters?affiliation=Fire+Nation')
   end
-
-
-
 
   private
 
-  def get_json(url, params = nil)
-    response = conn.get(url, params) do |request|
+  def get_json(url)
+    response = conn.get(url) do |request|
     end
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
-    Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1/characters') do |f|
+    Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1') do |f|
       f.adapter Faraday.default_adapter
     end
   end
--- spec/services/airbender_service_spec.rb
+++ spec/services/airbender_service_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 describe 'Airbender API' do
   it 'sends all characters from Fire Nation' do
     airbender = AirbenderService.new
-    repos = airbender.get_characters
 
-    
-    expect(repos).to be_a(Array)
-    expect(repos.first).to have_key(:name)
-    expect(repos.first).to have_key(:html_url)
+    characters = airbender.get_characters_from_fire_nation
+
+    expect(characters).to be_a(Array)
+    expect(characters.first).to have_key(:_id)
+    expect(characters.first).to have_key(:allies)
+    expect(characters.first).to have_key(:enemies)
+    expect(characters.first).to have_key(:photoUrl)
+    expect(characters.first).to have_key(:name)
+    expect(characters.first).to have_key(:affiliation)
   end
 end
